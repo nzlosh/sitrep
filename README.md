1f0ce99a0eb7fbc52057977928258becfb3824c1
Carlos <nzlosh@yahoo.com>
2016-11-06 20:21:43 +0100

    Added api call to fetch data and display it in the review tab. Updated README docs with software requirements.

--- README.md
+++ README.md
@@ -1,8 +1,65 @@
 # Situation Report (SitRep)
+A web application to help track Shinken alerts and administrator activities to resolve them.
+
 
 ## Project Status
-
 This project is currently a work in progress.
 
+
+## Insipration
+This tool was greatly inspired by the Opsweekly by Etsy.
+https://github.com/etsy/opsweekly
+
+
+## Installation
+The installation process is still very manual at this stage.
+
+### Backend - database and server daemon
+
+#### MySQL
+Install MySQL server on the host that will be used to store Shinken logs.  Set up the database
+schema.
+
+#### SitRep daemon
+Build the sitrep daemon and place it on the host that will be accessible by the frontend webapp.
+
+#### Nginx - Reverse proxy and web server
+Nginx is used as a reverse proxy to access the SitRep daemon's REST API.  It also servers the static
+content for the web app.  The following library/frameworks need to be included in the webroot.
+
+ - semantic.min.css
+ - cal-heatmap.css
+ - jquery-3.1.0.js
+ - semantic.min.js
+ - d3.v3.min.js
+ - cal-heatmap.min.js
+
+
+### Frontend - web application
+The web application requires access to SitRep daemon REST API.
+
+
+
 ## Legal
-Thanks to https://icons8.com for the use of their radar icon radar icon
+Thanks to https://icons8.com for the use of their icon
+
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2016 Carlos (nzlosh@yahoo.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- webui/sitrep.js
+++ webui/sitrep.js
@@ -1,4 +1,4 @@
-console.log("[siterep.js] loading.");
+console.log(Date() + "[siterep.js] loading.");
 
 // Activate dropdown menu.
 $(document).ready(function() {
@@ -18,7 +18,9 @@ $('.ui.pointing.menu .item').tab();
 
 // Semantic UI API map
 $.fn.api.settings.api = {
-    'get admins': '/api/admins'
+    "get admins": "/api/admins",
+    "get app version": "/api/version",
+    "get all alerts": "/api/alertlog",
 };
 
 // Admins dropdown menu.
@@ -35,9 +37,85 @@ $('.ui.dropdown.item').api({
     }
 });
 
-$('#start_date').calendar();
-$('#end_date').calendar();
+// Review Alerts
+$("#load_alertlog").api({
+    action: "get all alerts",
+    method: "GET",
+    onSuccess: function(response){
+        max_pages = 10;
+        page_size = response.length;
+        console.log("Items: " + page_size);
+        display_items = 0;
 
+        start_exec = new Date();
+
+        $("#review_table").empty();
+        response.forEach(function(r){
+
+            if ( display_items > parseInt(page_size/max_pages)) return;
+
+            if ( r.status == "CRITICAL" ) {
+                td_state = "class='error'";
+                label_state = "red";
+            } else if ( r.status == "WARNING" ) {
+                td_state = "class='warning'";
+                label_state = "orange";
+            } else if ( r.status == "OK" ) {
+                td_state = "class='positive'";
+                label_state = "green";
+            } else if ( r.status == "DOWN" ) {
+                td_state = "class='error'";
+                label_state = "pink";
+            } else if ( r.status == "UP" ) {
+                td_state = "class='positive'";
+                label_state = "teal";
+            } else {
+                td_state = "";
+                label_state = "grey";
+            }
+            $("#review_table").append("<tr id='"+r.id+"' " + td_state + "><td>" + new Date(r.alert_date*1000) + "</td>" +
+                "<td>" + r.host + "</td>" +
+                "<td>" + r.service + "</td>" +
+                "<td><i class='ui "+ label_state + " label'>"  + r.status + "</i></td>" +
+                "<td>" + r.output + "</td>" +
+            "</tr>");
+            display_items++;
+        });
+
+        console.log(parseInt(new Date() - start_exec));
+    }
+});
+
+
+// Calendar for alerting period
+$('#start_date').calendar({
+  monthFirst: false,
+  formatter: {
+    date: function (date, settings) {
+      if (!date) return '';
+      var day = date.getDate();
+      var month = date.getMonth() + 1;
+      var year = date.getFullYear();
+      return day + '/' + month + '/' + year;
+    }
+  }
+});
+
+$('#end_date').calendar({
+  monthFirst: false,
+  formatter: {
+    date: function (date, settings) {
+      if (!date) return '';
+      var day = date.getDate();
+      var month = date.getMonth() + 1;
+      var year = date.getFullYear();
+      return day + '/' + month + '/' + year;
+    }
+  }
+});
+
+
+// Application version from backend.
 $("#app_version").api({
     action: "get app version",
     onSuccess: function(response){
@@ -46,4 +124,4 @@ $("#app_version").api({
     }
 });
 
-console.log("[siterep.js] loaded.");
+console.log(Date() + "[siterep.js] loaded.");
